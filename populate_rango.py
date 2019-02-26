b2b9ee0d3ce1bdba4c8f8502350589bde6e2e4a0
007vict <super_vg@bk.ru>
2019-02-26 21:21:04 +0300

    change script

--- populate_rango.py
+++ populate_rango.py
@@ -14,39 +14,47 @@ def populate():
 
     add_page(cat=python_cat,
              title='Official Python Tutorial',
-             url="http://docs.python.org/2/tutorial/")
+             url="http://docs.python.org/2/tutorial/",
+             views=122)
 
     add_page(cat=python_cat,
              title="How to Think like a Computer Scientist",
-             url="http://www.greenteapress.com/thinkpython/")
+             url="http://www.greenteapress.com/thinkpython/",
+             views=100)
 
     add_page(cat=python_cat,
              title="Learn Python in 10 Minutes",
-             url="http://www.korokithakis.net/tutorials/python/")
+             url="http://www.korokithakis.net/tutorials/python/",
+             views=45)
 
     django_cat = add_cat("Django")
 
     add_page(cat=django_cat,
              title="Official Django Tutorial",
-             url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+             url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",
+             views=43)
 
     add_page(cat=django_cat,
              title="Django Rocks",
-             url="http://www.djangorocks.com/")
+             url="http://www.djangorocks.com/",
+             views=1345)
 
     add_page(cat=django_cat,
              title="How to Tango with Django",
-             url="http://www.tangowithdjango.com/")
+             url="http://www.tangowithdjango.com/",
+             views=76)
 
     frame_cat = add_cat("Other Frameworks")
 
     add_page(cat=frame_cat,
              title="Bottle",
-             url="http://bottlepy.org/docs/dev/")
+             url="http://bottlepy.org/docs/dev/",
+             views=87)
 
     add_page(cat=frame_cat,
              title="Flask",
-             url="http://flask.pocoo.org")
+             url="http://flask.pocoo.org",
+             views=98)
 
     # Выводим на экран пользователю то, что мы добавили в базу
     for c in Category.objects.all():
@@ -54,7 +62,7 @@ def populate():
             print("- {0} - {1}".format(str(c), str(p)))
 
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url = url
     p.views = views
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from rango.models import Category
+from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -11,5 +11,18 @@ def about(request):
     return render(request, 'rango/about.html', {'chameleon':
                                                     'Chameleons are adapted for '
                                                     'climbing and visual hunting.'})
+
+def category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'rango/category.html', context_dict)
 
 
