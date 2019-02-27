162c664f3f9326cfd09f7558f9d08285e9c9d203
007vict <super_vg@bk.ru>
2019-02-27 22:30:25 +0300

    create a new page view

--- rango/urls.py
+++ rango/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
-from django.conf.urls import url
 
 urlpatterns =[
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    re_path(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category')
+    path('category/<category_name_slug>/', views.category, name='category'),
+    path('add_category/', views.add_category, name='add_category'),
+    path('category/<category_name_slug>/add_page/', views.add_page, name='add_page'),
 ]
