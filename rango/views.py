1270b31e558436bd9516ca599dd4e2d4058a9e03
007vict <super_vg@bk.ru>
2019-02-28 13:48:02 +0300

    create base and inherut all templates

--- rango/views.py
+++ rango/views.py
@@ -116,7 +116,8 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    context = {'message': "Since you're logged in, you can see this text!"}
+    return render(request, 'rango/restricted.html', context)
 
 @login_required
 def user_logout(request):
